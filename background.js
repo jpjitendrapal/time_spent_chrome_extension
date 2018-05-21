5cd851252e701407d6ba95df6171be68ba20605d
jitendra <jitendra.pal@snapdeal.com>
2018-05-21 11:47:36 +0530

    time spent chrome extension code

---
+++ background.js
@@ -0,0 +1,50 @@
+var TIMERCOUNT = 2;
+
+function getHost(url){
+    var location = document.createElement("a");
+    location.href = url;
+    return location;
+}
+
+function processData(data, key){
+    
+    if(!data){
+        return  (key+"::"+TIMERCOUNT);
+    }
+    
+    var i=0, d, len, count=0, found = false;
+    d = data.split("|$|");
+    len = d.length;
+    for(i=0; i < len; i++){
+        if(d[i].indexOf(key) == 0){
+            count = d[i].split("::")[1];
+            d.splice(i, 1);
+            d.push(key + "::" + (TIMERCOUNT + parseInt(count)));
+            found = true;
+            break;
+        }
+    }
+    if(!found){
+        d.push(key+"::"+TIMERCOUNT);
+    }
+    data = d.join("|$|");
+    return data;
+}
+function getCurrentTabInfo() {
+    var currentValue;
+    setInterval(function () {
+        chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+            if(tabs.length < 1) return;
+            var key = tabs[0].url;
+            if(!key) return;
+            key = getHost(key).origin;
+            
+            currentValue = processData(currentValue, key);
+            
+            chrome.storage.local.set({ key : currentValue }, function () {});
+        });
+    }, 1000*TIMERCOUNT);
+}
+
+
+getCurrentTabInfo();
---
+++ popup.js
@@ -0,0 +1,33 @@
+
+function getTimeFromSec(sec){
+  var d = Number(sec);
+  var h = Math.floor(d / 3600);
+  var m = Math.floor(d % 3600 / 60);
+  var s = Math.floor(d % 3600 % 60);
+  return ('0' + h).slice(-2) + ":" + ('0' + m).slice(-2) + ":" + ('0' + s).slice(-2);
+}
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+  var _htm = '<table class="site-items" border="1">';
+
+  _htm += '<tr class="site-item"><th class="site"><b>Site</b></th> <th class="time"><b>Time Spent</b></th></tr>';
+
+  for (key in changes) {
+    var storageChange = changes[key];
+                // storageChange.oldValue
+
+                var sites = storageChange.newValue.split('|$|');
+                var len = sites.length-1, info;
+                for(var i = len; i >= 0; i--){
+                  info = sites[i].split('::');
+                  if(info[0].indexOf("chrome://newtab") == 0 || info[0].startsWith("null")){
+                    continue;
+                  }
+                  _htm += '<tr class="site-item"><td class="site">'+ info[0] + '</td> <td class="time">' + getTimeFromSec(info[1]) + '</td></tr>';    
+                }
+  }
+
+  _htm += '</table>'
+
+  document.getElementById('current-tab').innerHTML = _htm;
+});
+
